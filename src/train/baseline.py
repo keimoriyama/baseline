63eab975db24dc71ccab765e0e4888e8d202cafb
keimoriyama <keischwiiz@gmail.com>
2022-11-13 20:12:42 +0900

    change logger from wandb to mlflow

--- src/train/baseline.py
+++ src/train/baseline.py
@@ -51,13 +51,6 @@ def baseline_train(data_path, config):
     )
 
     # loggerの用意
-    """
-    wandb_logger = WandbLogger(name=exp_name, project="baseline")
-    wandb_logger.log_hyperparams(config.train)
-    wandb_logger.log_hyperparams(config.mode)
-    wandb_logger.log_hyperparams(config.seed)
-    wandb_logger.log_hyperparams(config.model)
-    """
     mlflow_logger = MLFlowLogger(experiment_name = exp_name)
     mlflow_logger.log_hyperparams(config.train)
     # import ipdb;ipdb.set_trace()
